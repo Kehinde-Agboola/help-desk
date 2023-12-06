d9d8fe09e31c9a66128886414d07f008041caf65
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-12-06 23:51:28 +0100

    loading suspense+fallback

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Suspense } from "react";
 import TicketList from "./TicketList";
+import Loading from "../loading";
 
 export default function Ticket() {
   return (
@@ -12,7 +14,9 @@ export default function Ticket() {
           </p>
         </div>
       </nav>
-      <TicketList />
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   );
 }
