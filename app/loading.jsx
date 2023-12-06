398f53e9b129d8b440258dbab98d176a234b5627
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-12-06 23:50:32 +0100

    added suspense.

---
+++ app/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div>
+      <main className="text-center">
+        <h2 className="text-primary"></h2>
+        <p>Hopefully not for so long :)</p>
+      </main>
+    </div>
+  );
+}
